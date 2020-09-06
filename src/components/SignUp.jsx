95876412ac1d1d85d07a6ad41e05a852254138b6
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2020-09-06 23:35:21 +0100

    first commit

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,262 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Typography,
+  TextField,
+  LinearProgress,
+  makeStyles,
+  CssBaseline,
+  Container,
+  Grid,
+  InputAdornment,
+  IconButton,
+  Card,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(3),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  error: {
+    color: "rgb(235, 54, 54)",
+  },
+}));
+
+const Signup = () => {
+  const [err, setErr] = useState(false);
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [severity, setSeverity] = useState("success");
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = () => setShowPassword(!showPassword);
+  //   let history = useHistory();
+  const classes = useStyles();
+
+  return (
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          console.log("Signing up", values);
+          setLoading(true);
+
+          axios
+            .post(`https://api.raisely.com/v3/signup`, {
+              campaignUuid: "46aa3270-d2ee-11ea-a9f0-e9a68ccff42a",
+              data: {
+                firstName: values.firstName,
+                lastName: values.lastName,
+                email: values.email,
+                password: values.password,
+              },
+            })
+            .then((res) => {
+              console.log(res.data.data.message);
+              setMessage(err);
+              setLoading(false);
+            })
+            .catch((err) => {
+              console.log(err);
+              setMessage("EXISTS");
+              setSeverity("error");
+              setErr(true);
+              setLoading(false);
+            });
+          setSubmitting(false);
+        }, 200);
+      }}
+      validationSchema={Yup.object().shape({
+        firstName: Yup.string()
+          .required("Required")
+          .min(2, "The first name can not be less than 2"),
+        lastName: Yup.string().required("Required"),
+        email: Yup.string().email("Invalid email").required("Required"),
+        password: Yup.string()
+          .required("No password provided")
+          .min(8)
+          .matches(
+            /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+            "Password must contain at least 8 characters,one lowercase, one uppercase, one number and one special case character"
+          ),
+      })}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        } = props;
+        return (
+          <>
+            <div style={{ marginBottom: "20vh" }}></div>
+            <Container component={Card} maxWidth="sm" elevation={7}>
+              <CssBaseline />
+              {err ? <Alert severity={severity}>{message}</Alert> : <div></div>}
+              <div className={classes.paper}>
+                <div className={classes.display}>
+                  <Typography
+                    component="h1"
+                    variant="h5"
+                    style={{ fontSize: 40, fontWeight: 500 }}
+                  >
+                    Welcome to Raisely
+                  </Typography>
+                </div>
+
+                <form onSubmit={handleSubmit} className={classes.form}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={12}>
+                      <TextField
+                        name="firstName"
+                        label="First Name*"
+                        fullWidth
+                        variant="outlined"
+                        type="text"
+                        error={err}
+                        value={values.firstName}
+                        className={
+                          errors.firstName && touched.firstName && "error"
+                        }
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      {errors.firstName && touched.firstName && (
+                        <div className={classes.error}>
+                          {" "}
+                          {errors.firstName}{" "}
+                        </div>
+                      )}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        name="lastName"
+                        label="Last Name*"
+                        fullWidth
+                        variant="outlined"
+                        type="text"
+                        multiline
+                        error={err}
+                        value={values.lastName}
+                        className={
+                          errors.lastName && touched.lastName && "error"
+                        }
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      {errors.lastName && touched.lastName && (
+                        <div className={classes.error}> {errors.lastName} </div>
+                      )}
+                    </Grid>
+
+                    <Grid item xs={12}>
+                      <TextField
+                        name="email"
+                        label="Email Address *"
+                        fullWidth
+                        variant="outlined"
+                        type="email"
+                        error={err}
+                        value={values.email}
+                        className={errors.email && touched.email && "error"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      {errors.email && touched.email && (
+                        <div className={classes.error}> {errors.email} </div>
+                      )}
+                    </Grid>
+
+                    <Grid item xs={12}>
+                      <TextField
+                        name="password"
+                        label="Password *"
+                        fullWidth
+                        type={showPassword ? "text" : "password"}
+                        variant="outlined"
+                        error={err}
+                        className={
+                          errors.password && touched.password && "error"
+                        }
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        InputProps={{
+                          // <-- This is where the toggle button is added.
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                              >
+                                {showPassword ? (
+                                  <Visibility />
+                                ) : (
+                                  <VisibilityOff />
+                                )}
+                              </IconButton>
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                      {errors.password && touched.password && (
+                        <div className={classes.error}> {errors.password} </div>
+                      )}
+                    </Grid>
+                  </Grid>
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    disabled={loading}
+                    className={classes.submit}
+                    onClick={handleSubmit}
+                    style={{ padding: 15 }}
+                  >
+                    Sign Up
+                  </Button>
+                  {loading && (
+                    <LinearProgress
+                      variant="query"
+                      style={{ marginTop: "10px" }}
+                    />
+                  )}
+                </form>
+              </div>
+            </Container>
+          </>
+        );
+      }}
+    </Formik>
+  );
+};
+export default Signup;
